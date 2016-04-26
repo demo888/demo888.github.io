f36ca10c47e9a8213560b01ede0ce2f299d0224a
郑馨洁龙 <915359637@qq.com>
2016-04-26 20:50:00 +0800

    11

---
+++ jyxqapp/files/uc4_4_面试邀约/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,[i,j,k,l,m,n,o,p,q],r,_(s,t,u,v,w,x,y,_(),z,_(A,B,C,D,E,_(F,G,H,I),J,null,K,D,L,D,M,N,O,null,P,Q,R,S,T,U,V,Q),W,_(),X,_(),Y,_(Z,[])),ba,_(),bb,_());}; 
+var b="url",c="uc4_4_面试邀约.html",d="generationDate",e=new Date(1461674914603.63),f="isCanvasEnabled",g=false,h="variables",i="OnLoadVariable",j="yemian",k="duilie",l="dengru",m="taolun",n="dengruchu",o="jianli",p="zhiwei",q="qiyezhuangtai",r="page",s="packageId",t="6cb48fd3d20b4afcae6094cfb45f3df9",u="type",v="Axure:Page",w="name",x="UC4.4 面试邀约",y="notes",z="style",A="baseStyle",B="627587b6038d43cca051c114ac41ad32",C="pageAlignment",D="near",E="fill",F="fillType",G="solid",H="color",I=0xFFFFFFFF,J="image",K="imageHorizontalAlignment",L="imageVerticalAlignment",M="imageRepeat",N="auto",O="favicon",P="sketchFactor",Q="0",R="colorStyle",S="appliedColor",T="fontName",U="Applied Font",V="borderWidth",W="adaptiveStyles",X="interactionMap",Y="diagram",Z="objects",ba="masters",bb="objectPaths";
+return _creator();
+})());
